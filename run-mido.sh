9b8c9df822f1dfdea340b56f70d8a3ee7943d969
Xavier León <xleon.mail@gmail.com>
2015-02-16 12:04:46 +0000

    Added mandatory tests

--- run-mido.sh
+++ run-mido.sh
@@ -1,17 +1,5 @@
-#!/bin/sh
-nosetests -v \
-tempest.scenario.midokura.test_network_advanced_inter_vmconnectivity \
-tempest.scenario.midokura.test_network_advanced_metadata.py \
-tempest.scenario.midokura.test_network_advanced_security_groups \
-tempest.scenario.midokura.test_network_advanced_security_groups_2networks \
-tempest.scenario.midokura.test_network_basic_adminstateup \
-tempest.scenario.midokura.test_network_basic_dhcp_disable \
-tempest.scenario.midokura.test_network_basic_dhcp_lease \
-tempest.scenario.midokura.test_network_basic_inter_vmconnectivity \
-tempest.scenario.midokura.test_network_basic_metadata \
-tempest.scenario.midokura.test_network_basic_multisubnet \
-tempest.scenario.midokura.test_network_basic_security_groups \
-tempest.scenario.midokura.test_network_basic_security_groups_2networks \
-tempest.scenario.midokura.test_network_basic_security_groups_netcat \
-tempest.scenario.midokura.test_network_basic_vmconnectivity \
-2>&1 | tee mido-results-`date +%Y%m%d-%H%M`.log
+./run_tempest.sh -t tempest.api.network \
+    tempest.scenario.test_network_basic_ops \
+    tempest.scenario.test_network_advanced_server_ops \
+    tempest.scenario.test_security_groups_basic_ops \
+    tempest.scenario.midokura 2>&1 | tee test_results_complete_$(date +%y%d%M-%H%m).log
