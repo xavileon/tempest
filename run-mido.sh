c981eae6456876673e3d3e48184ce100a22779fd
Xavier León <xleon.mail@gmail.com>
2015-02-18 09:25:01 +0000

    Added mandatory tests fix

--- run-mido.sh
+++ run-mido.sh
@@ -1,5 +1,12 @@
-./run_tempest.sh -t tempest.api.network \
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage ./run-mido.sh [tag]"
+    exit 1
+fi
+
+./run_tempest.sh tempest.api.network \
     tempest.scenario.test_network_basic_ops \
     tempest.scenario.test_network_advanced_server_ops \
     tempest.scenario.test_security_groups_basic_ops \
-    tempest.scenario.midokura 2>&1 | tee test_results_complete_$(date +%y%d%M-%H%m).log
+    tempest.scenario.midokura 2>&1 | tee test_results_$1_$(date +%Y%d%m-%H%M).log
